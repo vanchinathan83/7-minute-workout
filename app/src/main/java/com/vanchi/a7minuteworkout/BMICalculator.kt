f205c04796057652cb6474ae5bfdcdca5ce0b01b
vayipalayamchand <ac.vanchinathan@gmail.com>
2021-12-18 12:28:35 -0600

    Adding Metric system feature

--- app/src/main/java/com/vanchi/a7minuteworkout/BMICalculator.kt
+++ app/src/main/java/com/vanchi/a7minuteworkout/BMICalculator.kt
@@ -1,15 +1,20 @@
 package com.vanchi.a7minuteworkout
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.widget.TooltipCompat
 import com.vanchi.a7minuteworkout.databinding.ActivityBmicalculatorBinding
 import java.math.BigDecimal
 import java.math.RoundingMode
 
 class BMICalculator : AppCompatActivity() {
     var binding: ActivityBmicalculatorBinding? = null
+    var isUSSytem: Boolean = true
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBmicalculatorBinding.inflate(layoutInflater)
@@ -22,14 +27,35 @@ class BMICalculator : AppCompatActivity() {
         binding?.bmiToolbar?.setNavigationOnClickListener{
             onBackPressed()
         }
+        binding?.rgUnits?.setOnCheckedChangeListener { _, checkedId ->
+            if(checkedId.equals(R.id.rbUS)){
+                binding?.tilHeightInCms?.visibility = View.INVISIBLE
+                binding?.tilHeight?.visibility = View.VISIBLE
+                binding?.tilWeight?.hint = "Weight in LBS"
+                isUSSytem = true
+            }else{
+                binding?.tilHeightInCms?.visibility = View.VISIBLE
+                binding?.tilHeight?.visibility = View.INVISIBLE
+                binding?.tilWeight?.hint = "Weight in Kgs"
+                isUSSytem = false
+            }
+            resetBMIResultView()
+        }
 
         binding?.btnBMICalculate?.setOnClickListener{view ->
             if(isInputsValid()){
-                val height = binding?.etHeight?.text.toString().toFloat()
-                val weight = binding?.etWeight?.text.toString().toFloat()
+                if(isUSSytem) {
+                    val height = binding?.etHeight?.text.toString().toFloat()
+                    val weight = binding?.etWeight?.text.toString().toFloat()
 
-                val bmi = (weight / height / height) * 703
-                displayBMI(bmi)
+                    val bmi = (weight / height / height) * 703
+                    displayBMI(bmi)
+                }else{
+                    val height = binding?.etHeightInCms?.text.toString().toFloat()
+                    val weight = binding?.etWeight?.text.toString().toFloat()
+                    val bmi = (weight / height / height) * 10000
+                    displayBMI(bmi)
+                }
 
             }else{
                 Toast.makeText(this,"Please enter height and weight",Toast.LENGTH_SHORT).show()
@@ -37,7 +63,15 @@ class BMICalculator : AppCompatActivity() {
         }
     }
 
-    fun displayBMI(bmi: Float) {
+    private fun resetBMIResultView(){
+        binding?.tvBMIResult?.text = ""
+        binding?.tvBMIResultDescription?.text = ""
+        binding?.tvBMIValue?.text = ""
+        binding?.tilWeight?.editText?.text?.clear()
+        binding?.llBMIResult?.visibility = View.INVISIBLE
+    }
+
+    private fun displayBMI(bmi: Float) {
         var bmiResult : String
         var bmiResultDescription: String
         if(bmi.compareTo(18.0) < 0){
@@ -56,8 +90,10 @@ class BMICalculator : AppCompatActivity() {
         binding?.llBMIResult?.visibility = View.VISIBLE
     }
 
-    fun isInputsValid(): Boolean {
-        if(binding?.etHeight?.text.toString().isNotEmpty() && binding?.etWeight?.text.toString().isNotEmpty()){
+    private fun isInputsValid(): Boolean {
+        if(isUSSytem && binding?.etHeight?.text.toString().isNotEmpty() && binding?.etWeight?.text.toString().isNotEmpty()){
+            return true
+        } else if(!isUSSytem &&  binding?.etHeightInCms?.text.toString().isNotEmpty() && binding?.etWeight?.text.toString().isNotEmpty()){
             return true
         }
         return false
